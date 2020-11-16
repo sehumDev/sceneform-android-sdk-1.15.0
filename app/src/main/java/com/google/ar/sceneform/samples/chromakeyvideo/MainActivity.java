9f63dd5fab75454f11981991bb0f904cdf0e928f
ukgityes <ksug114@naver.com>
2020-11-16 16:52:36 +0900

    sunuk test3

--- app/src/main/java/com/google/ar/sceneform/samples/chromakeyvideo/MainActivity.java
+++ app/src/main/java/com/google/ar/sceneform/samples/chromakeyvideo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 테스트야
+        // 테스트야야야야
     }
 }
